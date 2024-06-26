d4aa4125b1d1a72af76332ce5afc658933cee062
YamrajOfcodes <kundanpatil0111@.gmail.com>
2024-06-26 21:23:25 +0530

    mini_project

---
+++ 03_miniProject/gsap.js
@@ -0,0 +1,16 @@
+ var time=gsap.timeline();
+
+
+ time.from("h1",{
+    y:-100,
+    duration:1,
+    opacity:0
+ })
+
+ time.from("li",{
+    y:-100,
+    duration:1,
+    stagger:1,
+    opacity:0
+
+ })
